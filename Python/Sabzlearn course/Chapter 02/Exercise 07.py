fdb4a1b9aaa9e6ee9ed55274db6583f235aa3441
Mahdi Sam <111331374+Mahdi-Sam@users.noreply.github.com>
2026-08-17 17:56:59 +0330

    exercise 07 from chapter 02 completed

---
+++ Python/Sabzlearn course/Chapter 02/Exercise 07.py
@@ -0,0 +1,7 @@
+# Write a Python program to calculate the area of ​​a triangle based on its base and height. Store the base and height in two variables, calculate the area, and print the result.
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+h = 10
+b = 12
+print("Area : ", h * b / 2)
