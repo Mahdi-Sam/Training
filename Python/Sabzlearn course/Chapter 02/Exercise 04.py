dfbd0ee923bc605054d3c74b5037b66ecc2f6c32
Mahdi Sam <111331374+Mahdi-Sam@users.noreply.github.com>
2026-08-17 12:33:12 +0330

    exercise 04 from chapter 02 completed

---
+++ Python/Sabzlearn course/Chapter 02/Exercise 04.py
@@ -0,0 +1,9 @@
+# Calculate the area and perimeter of a circle. Store the radius in a varable, then calculate and print the area and perimeter .
+
+# -----------------------------------------------------------------------------------------------------------------------------
+import math
+
+r = 6
+p = 2 * 3.14 * r
+a = math.pi * r**2
+print("r is 6 and p is", p, "and a is", round(a, 2))
