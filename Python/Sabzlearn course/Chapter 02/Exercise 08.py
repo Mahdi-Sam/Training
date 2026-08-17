28a6f3d52897c9e41fa5daac1add5e8d5ce163c9
Mahdi Sam <111331374+Mahdi-Sam@users.noreply.github.com>
2026-08-17 18:03:40 +0330

    Exercise 08 from chapter 02 completed

---
+++ Python/Sabzlearn course/Chapter 02/Exercise 08.py
@@ -0,0 +1,6 @@
+# Create a variable and store your age in it. Calculate the year in which you will turn 100. Print the result.
+
+# ------------------------------------------------------------------------------------------------------------
+
+age = 28
+print(f"in the year {2026 - age + 100} i'll become 100 years old")
