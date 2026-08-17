df18d9525b538cf029df4c7120c0adc4ffcf844d
Mahdi Sam <111331374+Mahdi-Sam@users.noreply.github.com>
2026-08-17 12:09:10 +0330

    exercise 03 from chapter 02 completed

---
+++ Python/Sabzlearn course/Chapter 02/Exercise 03.py
@@ -0,0 +1,7 @@
+# Create a program that converts temperature from Celsius to Fahrenheit. Store the temperature in Celsius in a variable, perform the conversion, and print the result.
+
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+c = 25
+f = (c * 1.8) + 32
+print(c, "\u00b0 celsius is ", f, "\u00b0 fahrenheit", sep="")
