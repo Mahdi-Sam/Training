4e182a3a49477f872b2fbb78c60aa927047b1fa8
Mahdi Sam <111331374+Mahdi-Sam@users.noreply.github.com>
2026-08-17 17:49:50 +0330

    exercise 06 from chapter 02 completed

---
+++ Python/Sabzlearn course/Chapter 02/Exercise 06.py
@@ -0,0 +1,6 @@
+# Write a program that calculates the average of three numbers. Define three variables (x, y, z), store the numbers in them, calculate the average, and print the result.
+
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+x, y, z = 17, 27, 12
+print("average of 17 , 27 , 12 = ", round((x + y + z) / 3 , 2))
