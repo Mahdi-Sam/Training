85e7388596579a3deb89b5b49f08bd03468a1501
Mahdi Sam <111331374+Mahdi-Sam@users.noreply.github.com>
2026-08-17 17:41:57 +0330

    exercise 05 from chapter 02 completed

---
+++ Python/Sabzlearn course/Chapter 02/Exercisee 05.py
@@ -0,0 +1,6 @@
+# Create a program that calculates the sum, difference, product, and power of two numbers (define them as variables x and y). Save the program, perform the calculations, and display the results.
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+x, y = 7, 3
+print(f"{x} + {y} = {x + y} \n{x} - {y} = {x - y} \n{x} * {y} = {x * y} \n{x} ** 2 = {x**2} \n{y} ** 2 = {y**2} \n")
