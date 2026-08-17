687ad80a38a67b40d3dec4bc345756d7137d679e
Mahdi Sam <111331374+Mahdi-Sam@users.noreply.github.com>
2026-08-17 18:21:55 +0330

    exercise 01 from chapter 03 / numbers completed

---
+++ Python/Sabzlearn course/Chapter 03/Numbers/Exercise 01.py
@@ -0,0 +1,6 @@
+# Write a program that takes the radius of a circle from the user and calculates its circumference and area.
+
+# ----------------------------------------------------------------------------------------------------------
+
+r = int(input("Enter radius : "))
+print(f"circumference is {round(2 * 3.14 * r, 2)} and area is {round(3.14 * r**2, 2)}")
