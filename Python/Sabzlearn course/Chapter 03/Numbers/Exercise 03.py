240dc4228c7d438bc55a4e3a1fe2b31a92027391
Mahdi Sam <111331374+Mahdi-Sam@users.noreply.github.com>
2026-08-18 11:57:15 +0330

    exercise 03 from chapter 03 / numbers completed

---
+++ Python/Sabzlearn course/Chapter 03/Numbers/Exercise 03.py
@@ -0,0 +1,8 @@
+# Write a program that takes two numbers from the user, raises the first to the power of the second, and prints the result.
+
+#--------------------------------------------------------------------------------------------------------------------------
+
+x = int(input("Enter a number : "))
+y = int(input("Enter another number : "))
+
+print(f"{x} ** {y} = {x ** y}")
