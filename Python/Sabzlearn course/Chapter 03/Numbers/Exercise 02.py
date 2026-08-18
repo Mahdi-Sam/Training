c3860e1110ac5da71ee4bd2fe3742ace25b6f8a8
Mahdi Sam <111331374+Mahdi-Sam@users.noreply.github.com>
2026-08-18 11:29:18 +0330

    exercise 02 from chapter 03 / numbers completed

---
+++ Python/Sabzlearn course/Chapter 03/Numbers/Exercise 02.py
@@ -0,0 +1,6 @@
+# Write a program that takes a number from the user and prints its square (power of 2) and cube (power of 3).
+
+# ----------------------------------------------------------------------------------------------------------
+
+x = int(input("Enter your number : "))
+print(f"The square of {x} is {x**2} and the cube of {x} is {x**3}")
