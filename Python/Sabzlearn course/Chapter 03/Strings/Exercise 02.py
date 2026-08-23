6e94058bb42d9d3280311f6f71adb726e9b85c1a
Mahdi Sam <111331374+Mahdi-Sam@users.noreply.github.com>
2026-08-23 09:32:15 +0330

    exercise 02 (unicode) from chapter 03 / strings completed

---
+++ Python/Sabzlearn course/Chapter 03/Strings/Exercise 02.py
@@ -0,0 +1,6 @@
+# Get a character from the user and print its Unicode value.
+
+# ----------------------------------------------------------
+
+x = input("Enter your character : ")
+print(x , "unicode is " , ord(x))
