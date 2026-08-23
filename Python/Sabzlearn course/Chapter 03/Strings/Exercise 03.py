fc7909cfc104c69e8535d345eeb4c23eb06c3590
Mahdi Sam <111331374+Mahdi-Sam@users.noreply.github.com>
2026-08-23 09:39:38 +0330

    Exercise 03 (isnumeric) from chapter 03 / string completed

---
+++ Python/Sabzlearn course/Chapter 03/Strings/Exercise 03.py
@@ -0,0 +1,6 @@
+# take a mobile number from the user and check whether all its digits are numeric.
+
+# --------------------------------------------------------------------------------
+
+x = input("Enter your phone number : ")
+print(x.isnumeric())
